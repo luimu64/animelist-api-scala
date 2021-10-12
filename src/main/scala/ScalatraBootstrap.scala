ff51136753bcf6a56ba71762d415407961de97c9
luimu64 <leimuraittinen@gmail.com>
2021-10-12 12:46:22 +0300

    cors

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -4,7 +4,7 @@ import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
-    context.setInitParameter("org.scalatra.cors.allowedOrigins", "http://localhost:8080")
     context.mount(new aniapi, "/*")
+    context.setInitParameter("org.scalatra.cors.allowedOrigins", "http://localhost:3000")
   }
 }
--- src/main/scala/com/anilist/api/aniapi.scala
+++ src/main/scala/com/anilist/api/aniapi.scala
@@ -2,8 +2,13 @@ package com.anilist.api
 
 import org.scalatra._
 import com.anilist.models._
+import org.scalatra.CorsSupport
 
-class aniapi extends ScalatraServlet {
+class aniapi extends ScalatraServlet with CorsSupport {
+
+  options("/*"){
+    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"))
+  }
 
   get("/v1/aniapi") {
     FetchAnimeData.getData
