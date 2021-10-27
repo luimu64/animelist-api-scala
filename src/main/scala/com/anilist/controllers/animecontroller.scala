7cf1b09f26c03e094f40431b47c504234044d0f6
luimu64 <leimuraittinen@gmail.com>
2021-10-27 22:20:17 +0300

    split format function and json definition

--- src/main/scala/com/anilist/controllers/animecontroller.scala
+++ src/main/scala/com/anilist/controllers/animecontroller.scala
@@ -7,25 +7,22 @@ import scala.collection.mutable.Buffer
 
 object AnimeController {
 
-  def formatAnimeDataToJson(animeData: Buffer[AnimeTitle]): String = {
-    //template for json structure
-    implicit val animeWrites: Writes[AnimeTitle] = (
-      (JsPath \ "id").write[Int] and
-        (JsPath \ "mal_id").write[Int] and
-        (JsPath \ "name").write[String] and
-        (JsPath \ "thumbnail").write[String] and
-        (JsPath \ "status").write[String] and
-        (JsPath \ "rating").write[String] and
-        (JsPath \ "reasoning").write[String] and
-        (JsPath \ "airing").write[Boolean] and
-        (JsPath \ "episodes").write[Int] and
-        (JsPath \ "score").write[Float] and
-        (JsPath \ "synopsis").write[String] and
-        (JsPath \ "url").write[String]
-      ) (unlift(AnimeTitle.unapply))
+  implicit val animeWrites: Writes[AnimeTitle] = (
+    (JsPath \ "id").write[Int] and
+      (JsPath \ "mal_id").write[Int] and
+      (JsPath \ "name").write[String] and
+      (JsPath \ "thumbnail").write[String] and
+      (JsPath \ "status").write[String] and
+      (JsPath \ "rating").write[String] and
+      (JsPath \ "reasoning").write[String] and
+      (JsPath \ "airing").write[Boolean] and
+      (JsPath \ "episodes").write[Int] and
+      (JsPath \ "score").write[Float] and
+      (JsPath \ "synopsis").write[String] and
+      (JsPath \ "url").write[String]
+    ) (unlift(AnimeTitle.unapply))
 
-    Json.stringify(Json.toJson(animeData))
-  }
+  def formatAnimeDataToJson(animeData: Buffer[AnimeTitle]): String = Json.stringify(Json.toJson(animeData))
 
   def getUserAnimelist(userid: String): String = {
     formatAnimeDataToJson(AnimeModel.getAnimeList(userid.toInt))
